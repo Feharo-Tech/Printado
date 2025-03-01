75028e9a84dc3f46b338ac7bc6cc3271e53dd168
Felipe Aquino <feaquino1993@gmail.com>
2025-03-01 14:25:37 -0300

    removendo window.show

--- printado/main.py
+++ printado/main.py
@@ -5,7 +5,6 @@ from printado.modules.gui import ScreenshotTool
 def main():
     app = QApplication(sys.argv)
     window = ScreenshotTool()
-    window.show()
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
