fe2126c40f090ce9613227c48ac15609c631669c
Felipe Aquino <feaquino1993@gmail.com>
2025-03-01 13:55:29 -0300

    instalador do pacote pip

--- printado/main.py
+++ printado/main.py
@@ -2,7 +2,11 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from printado.modules.gui import ScreenshotTool
 
-if __name__ == "__main__":
+def main():
     app = QApplication(sys.argv)
     window = ScreenshotTool()
+    window.show()
     sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
